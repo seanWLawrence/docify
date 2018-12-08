bdbaa932f5c1a8348a266510133facc51164eb61
Sean Lawrence <sean.lawrence@makeschool.com>
2018-12-08 17:16:06 -0600

    add basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
-  get "/about" => "static_pages#about"
-
   root "static_pages#index"
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get "signup", to: "users#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  get "logout", to: "sessions#destroy", as: "logout"
+
+  get "/about" => "static_pages#about"
 end
