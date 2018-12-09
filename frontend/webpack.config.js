10b7d8e1366bcf5a8db36b73508898a9c1f72694
Sean Lawrence <sean.lawrence@makeschool.com>
2018-12-09 16:04:26 -0600

    add typescript and start function

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,9 +1,23 @@
 let path = require('path');
 
 module.exports = {
-  entry: './src',
+  entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, '../app/assets/javascripts/editor'),
     filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-typescript', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
   },
 };
