22d515142700ec9781a1cf3290c19d2200ed951a
Sean Lawrence <sean.lawrence@makeschool.com>
2018-12-08 18:00:50 -0600

    create document scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181208210517) do
+ActiveRecord::Schema.define(version: 20181208232532) do
+
+  create_table "documents", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "private"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_documents_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
