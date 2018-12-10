75904c12ed3fb24f198b5cb263a1fee9420d818a
Sean Lawrence <sean.lawrence@makeschool.com>
2018-12-10 03:41:23 -0600

    add dante2 editor

---
+++ frontend/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import Dante from 'Dante2';
+import './index.css';
+
+let root = document.getElementById('root');
+
+render(<Dante />, root);
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,23 +1,56 @@
+let WebpackCleanupPlugin = require('webpack-cleanup-plugin');
 let path = require('path');
 
+let isDev = process.env.NODE_ENV === 'development';
+
 module.exports = {
-  entry: './src/index.ts',
+  mode: isDev ? 'development' : 'production',
+  entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, '../app/assets/javascripts/editor'),
-    filename: 'index.js',
+    chunkFilename: '[name].[contentHash].bundle.js',
+    filename: isDev ? 'index.js' : '[name].[contenthash].js',
+  },
+  resolve: {
+    extensions: ['.wasm', '.mjs', '.js', '.json', '.ts', '.tsx'],
   },
   module: {
     rules: [
       {
-        test: /\.tsx$/,
+        test: /\.ts?x$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-typescript', '@babel/preset-react'],
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-typescript',
+              '@babel/preset-react',
+            ],
+            plugins: [
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-syntax-dynamic-import',
+            ],
           },
         },
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  plugins: [new WebpackCleanupPlugin()],
 };
