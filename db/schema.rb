4ff1d21284feb0d1cdb93338f58a0e4f594e4cde
Sean Lawrence <sean.lawrence@makeschool.com>
2018-12-14 08:34:21 -0600

    change database to postgres for test and development

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 20181208232532) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "documents", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.boolean "private"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_documents_on_user_id"
@@ -30,4 +33,5 @@ ActiveRecord::Schema.define(version: 20181208232532) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "documents", "users"
 end
